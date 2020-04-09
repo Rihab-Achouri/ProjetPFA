4fbaabfeaa5d04490b9bed5ecc63a4b25b5a8fc7
Rihab-Achouri <rihabachouri45@gmail.com>
2020-04-09 18:10:09 +0200

    aaa

---
+++ DEL/reclamationDAO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.Data;
+using BEL;
+
+namespace DEL
+{
+    public class reclamationDAO
+    {
+
+    }
+}
--- ProjetPFA/Authentification.Designer.cs
+++ ProjetPFA/Authentification.Designer.cs
@@ -133,6 +133,7 @@
             this.button2.TabIndex = 10;
             this.button2.Text = "Quiter";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // pictureBox1
             // 
