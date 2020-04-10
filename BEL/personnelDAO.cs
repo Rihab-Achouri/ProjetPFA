5bc52d4e6a3e9307fabc17885680fa3f9feee3c8
pc <pc@DESKTOP-T2455K9>
2020-04-10 22:47:18 +0200

    test3

---
+++ BEL/personnelDAO.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+
+namespace BEL
+{
+    class personnelDAO
+    {
+
+        public static bool Insert_personnel(int id, string nom, string prenom, int tel, string adresse_mail, string post)
+        {
+            string requete = String.Format("insert into personnel (ID, nom,prenom,tel, adresse_mail, post)" +
+                " values ('{0}','{1}','{2}','{3}','{4}',{5}');", id, nom, prenom, tel, adresse_mail, post);
+            return utils.miseajour(requete);
+        }
+
+        public static bool Update_personnel(int id, string nom, string prenom, int tel, string adresse_mail, string post)
+        {
+            string requete = String.Format("update personnel set nom='{0}', prenom='{1}'," +
+                " tel='{2}', adresse_mail='{3}', post='{4}' where ID={5};", nom, prenom, tel, adresse_mail, post, id);
+            return utils.miseajour(requete);
+        }
+
+        public static bool Delete_personnel(int id)
+        {
+            string requete = String.Format("delete from personnel where ID={0};", id);
+            return utils.miseajour(requete);
+        }
+
+        public static personnel Get_personnel_ID(int id)
+        {
+            string requete = String.Format("select * from personnel where ID={0};", id);
+            OleDbDataReader rd = utils.lire(requete);
+            personnel c = new personnel();
+            if (rd.HasRows)
+            {
+                while (rd.Read())
+                {
+                    c.ID = rd.GetInt32(0);
+
+                    c.prenom = rd.GetString(1);
+                    c.nom = rd.GetString(2);
+                    c.tel = rd.GetInt32(3);
+                    c.adresse_mail = rd.GetString(4);
+                    c.post = rd.GetString(5);
+                }
+
+            }
+            utils.Disconnect();
+            return c;
+        }
+
+        public static List<personnel> Get_personnel()
+        {
+            string requete = String.Format("select * from personnel;");
+            OleDbDataReader rd = utils.lire(requete);
+            List<personnel> L = new List<personnel>();
+            personnel c;
+            while (rd.Read())
+            {
+                c = new personnel
+                {
+                    ID = rd.GetInt32(0),
+                    nom = rd.GetString(1),
+                    prenom = rd.GetString(2),
+                    tel = rd.GetInt32(3),
+                    adresse_mail = rd.GetString(4),
+                    post = rd.GetString(5),
+                };
+                L.Add(c);
+            }
+            utils.Disconnect();
+            return L;
+
+        }
+    }
+}
---
+++ BEL/utils.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.IO;
+using System.Reflection;
+using System.Configuration;
+
+namespace BEL
+{
+    public static class utils
+    {
+
+            public static OleDbConnection cn;
+            public static OleDbCommand cmd;
+            public static OleDbDataReader rd;
+            public static OleDbDataAdapter oledbAdapter;
+
+
+            public static void Connect()
+            {
+                string cnstr = "provider = Microsoft.JET.OLEDB.4.0;" +
+                    "Data Source =C:\\Users\\HP\\Desktop\\cabinetdentaire.mdb;";
+                cn = new OleDbConnection(cnstr);
+                cn.Open();
+            }
+            public static string Connect1()
+            {
+                string cnstr = "provider = Microsoft.JET.OLEDB.4.0;" +
+                    "Data Source =C:\\Users\\HP\\Desktop\\cabinetdentaire.mdb;";
+                return cnstr;
+            }
+
+
+
+
+            public static OleDbDataReader lire(string req)
+            {
+                Connect();
+                cmd = new OleDbCommand(req, cn);
+                rd = cmd.ExecuteReader();
+                return rd;
+            }
+
+            public static bool miseajour(string req)
+            {
+                Connect();
+                cmd = new OleDbCommand(req, cn);
+                int nbaffected = cmd.ExecuteNonQuery();
+                Disconnect();
+                if (nbaffected != 0)
+                    return true;
+                else
+                    return false;
+            }
+
+            public static void Disconnect()
+            {
+                cn.Close();
+                cn.Dispose();
+            }
+        
+    }
+
+}
+
