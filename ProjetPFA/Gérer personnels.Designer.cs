1afe4a1fd739535aa02f164fe2a3494c709bd7b0
pc <pc@DESKTOP-T2455K9>
2020-04-11 20:03:59 +0200

    test6

--- ProjetPFA/Gérer personnels.Designer.cs
+++ ProjetPFA/Gérer personnels.Designer.cs
@@ -80,7 +80,7 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, -2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(280, 86);
+            this.pictureBox1.Size = new System.Drawing.Size(276, 99);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 6;
             this.pictureBox1.TabStop = false;
---
+++ ProjetPFA/Gérer_clients.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetPFA
+{
+    public partial class Gérer_clients : Form
+    {
+        public Gérer_clients()
+        {
+            InitializeComponent();
+        }
+
+        private void Gérer_clients_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
