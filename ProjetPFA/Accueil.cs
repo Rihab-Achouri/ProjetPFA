3cb3ac2e7129e786e3ac48bd5ce96e12773e4457
Rihab-Achouri <rihabachouri45@gmail.com>
2020-04-11 02:43:32 +0200

    gy

--- ProjetPFA/Accueil.cs
+++ ProjetPFA/Accueil.cs
@@ -16,5 +16,10 @@ namespace ProjetPFA
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
