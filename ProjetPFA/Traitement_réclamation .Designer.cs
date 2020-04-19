ee241e75b279970078ce24088ff3479d94fe43d6
pc <pc@DESKTOP-T2455K9>
2020-04-19 21:56:27 +0200

    test13

--- ProjetPFA/Traitement_réclamation .Designer.cs
+++ ProjetPFA/Traitement_réclamation .Designer.cs
@@ -31,7 +31,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Traitement_personnel));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
             this.label6 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
@@ -63,6 +62,7 @@
             this.button8 = new System.Windows.Forms.Button();
             this.label9 = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
@@ -82,7 +82,6 @@
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.vScrollBar2);
             this.groupBox1.Controls.Add(this.label6);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label4);
@@ -97,19 +96,12 @@
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox1.ForeColor = System.Drawing.Color.Red;
-            this.groupBox1.Location = new System.Drawing.Point(12, 97);
+            this.groupBox1.Location = new System.Drawing.Point(12, 110);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(296, 314);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Réclamation arrivées";
-            // 
-            // vScrollBar2
-            // 
-            this.vScrollBar2.Location = new System.Drawing.Point(248, 212);
-            this.vScrollBar2.Name = "vScrollBar2";
-            this.vScrollBar2.Size = new System.Drawing.Size(12, 93);
-            this.vScrollBar2.TabIndex = 33;
             // 
             // label6
             // 
@@ -142,7 +134,7 @@
             // 
             this.richTextBox1.Location = new System.Drawing.Point(151, 209);
             this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.Size = new System.Drawing.Size(124, 96);
             this.richTextBox1.TabIndex = 8;
             this.richTextBox1.Text = "";
             // 
@@ -150,35 +142,35 @@
             // 
             this.textBox5.Location = new System.Drawing.Point(151, 170);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(100, 23);
+            this.textBox5.Size = new System.Drawing.Size(124, 23);
             this.textBox5.TabIndex = 7;
             // 
             // textBox4
             // 
             this.textBox4.Location = new System.Drawing.Point(151, 132);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 23);
+            this.textBox4.Size = new System.Drawing.Size(124, 23);
             this.textBox4.TabIndex = 6;
             // 
             // textBox3
             // 
             this.textBox3.Location = new System.Drawing.Point(151, 94);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 23);
+            this.textBox3.Size = new System.Drawing.Size(124, 23);
             this.textBox3.TabIndex = 5;
             // 
             // textBox2
             // 
             this.textBox2.Location = new System.Drawing.Point(151, 57);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 23);
+            this.textBox2.Size = new System.Drawing.Size(124, 23);
             this.textBox2.TabIndex = 4;
             // 
             // textBox1
             // 
             this.textBox1.Location = new System.Drawing.Point(151, 26);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.Size = new System.Drawing.Size(124, 23);
             this.textBox1.TabIndex = 3;
             // 
             // label3
@@ -217,24 +209,24 @@
             this.groupBox2.Controls.Add(this.richTextBox2);
             this.groupBox2.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.ForeColor = System.Drawing.Color.MediumBlue;
-            this.groupBox2.Location = new System.Drawing.Point(326, 110);
+            this.groupBox2.Location = new System.Drawing.Point(317, 123);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(239, 212);
+            this.groupBox2.Size = new System.Drawing.Size(245, 212);
             this.groupBox2.TabIndex = 3;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Décision";
             // 
             // vScrollBar3
             // 
-            this.vScrollBar3.Location = new System.Drawing.Point(200, 25);
+            this.vScrollBar3.Location = new System.Drawing.Point(222, 22);
             this.vScrollBar3.Name = "vScrollBar3";
-            this.vScrollBar3.Size = new System.Drawing.Size(12, 93);
+            this.vScrollBar3.Size = new System.Drawing.Size(14, 143);
             this.vScrollBar3.TabIndex = 34;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(29, 137);
+            this.label8.Location = new System.Drawing.Point(15, 180);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(37, 16);
             this.label8.TabIndex = 14;
@@ -243,7 +235,7 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(29, 25);
+            this.label7.Location = new System.Drawing.Point(15, 35);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(62, 16);
             this.label7.TabIndex = 13;
@@ -251,23 +243,23 @@
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(97, 130);
+            this.textBox6.Location = new System.Drawing.Point(83, 177);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(115, 23);
+            this.textBox6.Size = new System.Drawing.Size(153, 23);
             this.textBox6.TabIndex = 12;
             // 
             // richTextBox2
             // 
-            this.richTextBox2.Location = new System.Drawing.Point(97, 22);
+            this.richTextBox2.Location = new System.Drawing.Point(83, 22);
             this.richTextBox2.Name = "richTextBox2";
-            this.richTextBox2.Size = new System.Drawing.Size(115, 96);
+            this.richTextBox2.Size = new System.Drawing.Size(153, 143);
             this.richTextBox2.TabIndex = 11;
             this.richTextBox2.Text = "";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Maiandra GD", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(326, 341);
+            this.button1.Location = new System.Drawing.Point(344, 392);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(102, 32);
             this.button1.TabIndex = 15;
@@ -277,7 +269,7 @@
             // button2
             // 
             this.button2.Font = new System.Drawing.Font("Maiandra GD", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(326, 379);
+            this.button2.Location = new System.Drawing.Point(344, 445);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(102, 32);
             this.button2.TabIndex = 20;
@@ -287,17 +279,18 @@
             // button3
             // 
             this.button3.Font = new System.Drawing.Font("Maiandra GD", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(450, 379);
+            this.button3.Location = new System.Drawing.Point(460, 445);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(102, 32);
             this.button3.TabIndex = 21;
             this.button3.Text = "Supprimer";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
             this.button4.Font = new System.Drawing.Font("Maiandra GD", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(450, 341);
+            this.button4.Location = new System.Drawing.Point(460, 392);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(102, 32);
             this.button4.TabIndex = 22;
@@ -307,18 +300,19 @@
             // button5
             // 
             this.button5.Font = new System.Drawing.Font("Maiandra GD", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button5.Location = new System.Drawing.Point(605, 459);
+            this.button5.Location = new System.Drawing.Point(603, 236);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(360, 28);
             this.button5.TabIndex = 23;
             this.button5.Text = "Afficher la liste de toutes les réclamations";
             this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(605, 287);
+            this.dataGridView1.Location = new System.Drawing.Point(603, 270);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(360, 166);
             this.dataGridView1.TabIndex = 24;
@@ -326,7 +320,7 @@
             // 
             // vScrollBar1
             // 
-            this.vScrollBar1.Location = new System.Drawing.Point(945, 287);
+            this.vScrollBar1.Location = new System.Drawing.Point(943, 270);
             this.vScrollBar1.Name = "vScrollBar1";
             this.vScrollBar1.Size = new System.Drawing.Size(20, 166);
             this.vScrollBar1.TabIndex = 25;
@@ -336,7 +330,7 @@
             this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
             this.pictureBox2.Location = new System.Drawing.Point(603, 42);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(363, 194);
+            this.pictureBox2.Size = new System.Drawing.Size(347, 183);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 26;
             this.pictureBox2.TabStop = false;
@@ -344,7 +338,7 @@
             // button6
             // 
             this.button6.Font = new System.Drawing.Font("Maiandra GD", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button6.Location = new System.Drawing.Point(605, 247);
+            this.button6.Location = new System.Drawing.Point(603, 449);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(360, 28);
             this.button6.TabIndex = 27;
@@ -356,7 +350,7 @@
             this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button7.Font = new System.Drawing.Font("Maiandra GD", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button7.ForeColor = System.Drawing.Color.DarkRed;
-            this.button7.Location = new System.Drawing.Point(173, 459);
+            this.button7.Location = new System.Drawing.Point(151, 479);
             this.button7.Name = "button7";
             this.button7.Size = new System.Drawing.Size(102, 32);
             this.button7.TabIndex = 28;
@@ -368,7 +362,7 @@
             // 
             this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button8.Font = new System.Drawing.Font("Maiandra GD", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.Location = new System.Drawing.Point(12, 459);
+            this.button8.Location = new System.Drawing.Point(12, 479);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(102, 32);
             this.button8.TabIndex = 29;
@@ -398,11 +392,22 @@
             this.label11.TabIndex = 32;
             this.label11.Text = "meilleure source d\'apprentissage\"";
             // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Maiandra GD", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(603, 483);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(360, 28);
+            this.button9.TabIndex = 33;
+            this.button9.Text = "Afficher la liste des réclamations anullées";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
             // Traitement_personnel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(977, 503);
+            this.ClientSize = new System.Drawing.Size(977, 523);
+            this.Controls.Add(this.button9);
             this.Controls.Add(this.label11);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.button8);
@@ -468,7 +473,7 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label11;
-        private System.Windows.Forms.VScrollBar vScrollBar2;
         private System.Windows.Forms.VScrollBar vScrollBar3;
+        private System.Windows.Forms.Button button9;
     }
 }
